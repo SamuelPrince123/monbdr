4ed5897107fbcad5d6d0cf9b67b7842a9bebd164
SamuelPrince123 <st659136@gmail.com>
2026-05-06 19:40:08 +0600

    added

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const menuBtn = document.getElementById("menu-toggle");
 const dashboard = document.getElementById("dashboard");
 const mobileCenter = document.getElementById("mobile-center");
+const closeBtn = document.getElementById("close-btn");
 
 menuBtn.addEventListener("click", () => {
-  dashboard.classList.toggle("show");
-  mobileCenter.classList.toggle("hide");
+  dashboard.classList.add("show");
+  mobileCenter.classList.add("hide");
+});
+
+closeBtn.addEventListener("click", () => {
+  dashboard.classList.remove("show");
+  mobileCenter.classList.remove("hide");
 });
